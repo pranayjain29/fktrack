83a066c5e7044891e427ba3a48c72cf80ab23095
Pranay Jain <98742685+pranayjain29@users.noreply.github.com>
2024-07-19 15:36:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,16 +53,16 @@ def scrape_blinkit_search(FSN_list):
             review_element = soup.find('span', class_='Wphh3N')
             review = review_element.text.strip() if review_element else 'N/A'
             
-            rating_count = 'N/A'
-            review_count = 'N/A'
+            rating_count = 0
+            review_count = 0
             
             match = re.search(r'(\d{1,3}(?:,\d{3})*|\d+)\s*Ratings\s*&\s*(\d{1,3}(?:,\d{3})*|\d+)\s*Reviews', review)
             if match:
                 rating_count = int(match.group(1).replace(',', ''))
                 review_count = int(match.group(2).replace(',', ''))
             
-            rating_count = int(rating_count) if rating_count != 'N/A' else 'N/A'
-            review_count = int(review_count) if review_count != 'N/A' else 'N/A'
+            rating_count = int(rating_count) if rating_count != 'N/A' else 0
+            review_count = int(review_count) if review_count != 'N/A' else 0
 
             seller_element = soup.find(id="sellerName")
             seller_name = seller_element.text.strip() if seller_element else None
@@ -70,12 +70,11 @@ def scrape_blinkit_search(FSN_list):
             all_data.append({'FSN': FSN, 'TITLE': title, 'Price': price, 'Ratings': rating, 
                              'Rating Count': rating_count, 'Review Count': review_count, 'SOLD OUT': sold_out, 
                              'SELLER NAME': seller_name})
+            progress = (idx + 1) / total_fsns * 100
         except Exception as e:
             print(f"Error occurred for FSN: {FSN}. Error: {e}")
+            progress = (idx + 1) / total_fsns * 100
             continue
-
-        # Update progress
-        progress = (idx + 1) / total_fsns * 100
 
     driver.quit()
     df = pd.DataFrame(all_data)
